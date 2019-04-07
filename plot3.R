829edeca8c5383d9f6ee1e4262c0186b1c25c3ca
vickyunbc <48810660+vickyunbc@users.noreply.github.com>
2019-04-07 02:52:32 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,27 +1,25 @@
-#### This code will read, plot and save the final output in the png format for the plot 3 of week 1 project of Exploratory Data 
-#### Analysis course at coursera.
-
 # Reading, naming and subsetting power consumption data
-power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+ddata <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(ddata) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subdata <- subset(ddata,ddata$Date=="1/2/2007" | ddata$Date =="2/2/2007")
 
 # Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
-subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
-subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
-subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
-subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+subdata$Date <- as.Date(subdata$Date, format="%d/%m/%Y")
+subdata$Time <- strptime(subdata$Time, format="%H:%M:%S")
+subdata[1:1440,"Time"] <- format(subdata[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subdata[1441:2880,"Time"] <- format(subdata[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 dev.print(png, file = "plot3.png", width = 480, height = 480)
 png(file = "plot3.png", bg = "transparent")
 
 # calling the basic plot functions
-plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
-with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
-with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
-with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+plot(subdata$Time,subdata$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subdata,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subdata,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subdata,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 # annotating graph
 title(main="Energy sub-metering")
 dev.off()
+
